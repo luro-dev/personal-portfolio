6a8e1e14066e91071947892c8a1b36c887f34e19
Luis <106412395+luro-dev@users.noreply.github.com>
2025-10-09 12:00:10 -0400

    Finish utils.css class & header section

--- src/main.js
+++ src/main.js
@@ -1,6 +1,4 @@
 import "../styles/modern-normalize.css";
 import "../styles/style.css";
+import "../styles/components/header.css";
 import "../styles/utils.css";
-
-// Left video off at 25:33
-// working currently on media queries.
