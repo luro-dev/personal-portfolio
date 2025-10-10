317d41e1b49ae3a77b8c22b8149690ff98a8bbff
Luis <106412395+luro-dev@users.noreply.github.com>
2025-10-10 13:45:20 -0400

    Finish hero section

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 import "../styles/modern-normalize.css";
 import "../styles/style.css";
 import "../styles/components/header.css";
+import "../styles/components/hero.css";
 import "../styles/utils.css";
+
+// left off at hero section (44:38)
