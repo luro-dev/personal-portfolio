9efe804a3f8e8704204da7d6cbd82566082d1fb6
Luis <106412395+luro-dev@users.noreply.github.com>
2025-10-12 15:36:44 -0400

    Add about section & start responsive design

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ import "../styles/modern-normalize.css";
 import "../styles/style.css";
 import "../styles/components/header.css";
 import "../styles/components/hero.css";
+import "../styles/components/about.css";
 import "../styles/utils.css";
 
-// left off at hero section (44:38)
+// left off -> about (1:02:22) making about section responsive
