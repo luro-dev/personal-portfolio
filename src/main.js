8c43fabd20e531dc1bb244f931c19dc85cabaa35
Luis <106412395+luro-dev@users.noreply.github.com>
2025-10-13 11:50:23 -0400

    Finish about section & start featured section

--- src/main.js
+++ src/main.js
@@ -3,6 +3,7 @@ import "../styles/style.css";
 import "../styles/components/header.css";
 import "../styles/components/hero.css";
 import "../styles/components/about.css";
+import "../styles/components/featured.css";
 import "../styles/utils.css";
 
-// left off -> about (1:02:22) making about section responsive
+// left off -> featured section tech stack (1:19:31)
