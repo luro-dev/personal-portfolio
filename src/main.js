be5dbede5e1636efde6592ed209410e755abda59
Luis <106412395+luro-dev@users.noreply.github.com>
2025-10-15 12:12:57 -0400

    Finish project section

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import "../styles/components/header.css";
 import "../styles/components/hero.css";
 import "../styles/components/about.css";
 import "../styles/components/featured.css";
+import "../styles/components/work.css";
 import "../styles/utils.css";
 
-// left off -> featured section tech stack (1:19:31)
+// left off -> project section (1:37:58)
