bb129636c222f21a4b3516f8fbbcb6891b921ed9
Luis <106412395+luro-dev@users.noreply.github.com>
2025-10-20 12:00:28 -0400

    Finish mobile menu functionality

--- src/utils/mobile-nav.js
+++ src/utils/mobile-nav.js
@@ -3,8 +3,28 @@ const mobileNav = () => {
 
   const mobileNav = document.querySelector(".mobile-nav");
 
+  const mobileLinks = document.querySelectorAll(".mobile-nav__link");
+
+  // State -> mobile nav open or not?
+  let isMobileNavOpen = false;
+
   headerBtn.addEventListener("click", () => {
-    mobileNav.style.display = "flex";
+    isMobileNavOpen = !isMobileNavOpen;
+    if (isMobileNavOpen) {
+      mobileNav.style.display = "flex";
+      document.body.style.overflowY = "hidden";
+    } else {
+      mobileNav.style.display = "none";
+      document.body.style.overflowY = "auto";
+    }
+  });
+
+  mobileLinks.forEach((link) => {
+    link.addEventListener("click", () => {
+      isMobileNavOpen = !isMobileNavOpen;
+      mobileNav.style.display = "none";
+      document.body.style.overflowY = "auto";
+    });
   });
 };
 
